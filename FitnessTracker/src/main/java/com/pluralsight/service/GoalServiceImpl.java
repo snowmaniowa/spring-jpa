b1a9e536d462bca0f3e5eae00806e1fa85c1b791
snowmaniowa <santharaam@gmail.com>
2018-03-05 21:22:14 -0600

    Lot of JPA and some hibernate

---
+++ FitnessTracker/src/main/java/com/pluralsight/service/GoalServiceImpl.java
@@ -0,0 +1,33 @@
+package com.pluralsight.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pluralsight.model.Goal;
+import com.pluralsight.model.GoalReport;
+import com.pluralsight.repository.GoalRepository;
+
+@Service("goalService")
+public class GoalServiceImpl implements GoalService {
+
+	//This is important step in a service
+	@Autowired
+	private GoalRepository goalRepository;
+	
+	@Transactional
+	public Goal save(Goal goal) {
+		return goalRepository.save(goal);
+	}
+
+	public List<Goal> findAllGoals() {
+		return goalRepository.findAll();
+	}
+
+	public List<GoalReport> findAllGoalReports() {
+		return goalRepository.findAllGoalReports();
+	}
+
+}
